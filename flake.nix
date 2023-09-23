0d03c51662056969c218179d51f157be8fc611ea
WhiteBlackGoose <wbg@angouri.org>
2023-09-23 15:54:25 +0200

    nix fmt

--- flake.nix
+++ flake.nix
@@ -4,35 +4,42 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { nixpkgs, ... }:
-      let 
-        systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ]; 
-      in {
-        devShells = nixpkgs.lib.genAttrs systems (system: 
-        let 
-          pkgs = nixpkgs.legacyPackages.${system}; in
+    let
+      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
+    in
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
         {
           default =
-          pkgs.mkShell {
-            buildInputs = with pkgs; [
-              cargo
-              rustc
-              rust-analyzer
-              vscode-extensions.vadimcn.vscode-lldb
+            pkgs.mkShell {
+              buildInputs = with pkgs; [
+                cargo
+                rustc
+                rust-analyzer
+                vscode-extensions.vadimcn.vscode-lldb
 
-              cmake pkg-config fontconfig
-              gcc
-              libglvnd
-              libclang
-            ] ++ (with pkgs.xorg; [
-              libX11 libX11.dev
-              libXi
-              libXcursor
-              libXrandr
-              libXft libXft.dev
-              libXinerama
-            ]);
-            VSCODE_CODELLDB = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}";
-            LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${ with pkgs; lib.makeLibraryPath [
+                cmake
+                pkg-config
+                fontconfig
+                gcc
+                libglvnd
+                libclang
+              ] ++ (with pkgs.xorg; [
+                libX11
+                libX11.dev
+                libXi
+                libXcursor
+                libXrandr
+                libXft
+                libXft.dev
+                libXinerama
+              ]);
+              VSCODE_CODELLDB = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}";
+              LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${ with pkgs; lib.makeLibraryPath [
               libGL
               xorg.libX11
               xorg.libXi
@@ -42,13 +49,14 @@
               libclang
               clang
             ] }:/run/opengl-driver/lib";
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-          };
+              LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            };
         });
 
-        packages = nixpkgs.lib.genAttrs systems (system: 
-        let 
-          pkgs = nixpkgs.legacyPackages.${system}; in
+      packages = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
         {
           default = nixpkgs.legacyPackages.${system}.rustPlatform.buildRustPackage {
             pname = "frex";
@@ -73,6 +81,6 @@
               mainProgram = "frex";
             };
           };
-      });
+        });
     };
 }
